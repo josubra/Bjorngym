04785886a6d2407160cba3f0c2c5ed711be4a1da
Josué Ferreira Teodoro <josueftbr@gmail.com>
2019-04-10 14:58:15 -0300

    init commit

---
+++ Bjorngym/App.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Bjorngym.Services;
+using Bjorngym.Views;
+
+namespace Bjorngym
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+            //Background color
+            MainPage.SetValue(NavigationPage.BarBackgroundColorProperty, Color.Black);
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Bjorngym/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Bjorngym.Models;
+
+namespace Bjorngym.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
